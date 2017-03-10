6fa10ef525999deab34d795d5966a23613562259
Greg Benson <benson@usfca.edu>
2017-03-10 00:31:40 +0000

    More imm.s examples.

--- week07/imm.s
+++ week07/imm.s
@@ -2,8 +2,12 @@
 .func testimm
 
 testimm:
+        push {r4, r5}
+	//mov r0, #1023 bad
+	//mov r0, #4095 bad
+	mov r0, #1536 // 1024 + 512 good
 	mov r0, #1024
 	sub r1, r0, #1
-	//mov r1, #1023
+        pop {r4, r5}
 	bx lr
 
